b8d0d3aa5e2a2c2cbdf464e748d258132ec40229
SHARAN-RH <128074302+SHARAN-RH@users.noreply.github.com>
2024-12-30 11:40:34 +0530

    Update alert.sh

--- bin/alert.sh
+++ bin/alert.sh
@@ -2,4 +2,4 @@
 
 message=$1
 # Send an email or system notification (You can replace this with any notification system like Slack, Email, etc.)
-echo "ALERT: $message" | mail -s "Docker Resource Alert" sharanrhiremani@gmail.com
+echo "ALERT: $message" | mail -s "Docker Resource Alert" abc@abc.com
